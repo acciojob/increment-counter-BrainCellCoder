cc12e8a3d70ae2efca52bbc0708706bf8500f77f
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-19 11:12:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const counterElement = document.getElementById('counter');
+const incrementBtn = document.getElementById('incrementBtn');
+let counterValue = parseInt(counterElement.textContent, 10);
+function incrementCounter() {
+    alert(counterValue);
+    counterValue++;
+    counterElement.textContent = counterValue;
+}
+incrementBtn.addEventListener('click', incrementCounter);
